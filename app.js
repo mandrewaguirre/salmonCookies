ae792cd1caae844491141120c8d68371e09812ba
Miguel Andrew Aguirre <miguelandrewaguirre@gmail.com>
2016-04-19 12:48:08 -0700

    Fixed object pike, and added a method to append to html, still working on this method to eanble it to work.

--- app.js
+++ app.js
@@ -1,23 +1,8 @@
 var hoursOpen = ['6am', '7am ','8am ','9am ','10am ','11am ','12pm ','1pm ','2pm ','2pm ','4pm ','5pm ', '6pm ', '7pm ', '8pm ', '9pm '];
-var dailyTotalCookies = 'Totals: ';
-var hoursCookies = [];
 
 var totalCookies = 0;
-// totalsInil.textContent = 'totalsInil';
-//
-// for (var printLi = 0; printLi < hoursOpen.length; printLi++) {
-//   var printLitoHtml = document.createElement ('li');
-//   printLitoHtml.textContent = hoursCookies[printLi];
-//   totalsInil.appenChild(printLitoHtml);
-// };
-// liForTotals.textContent = hoursCookies;
-// document.totalsInUlel.appendChild(hoursCookies);
-// 1. Create el method to create element. 2. write on el the text content, 3. append to elemnt on page.
-
-var neLiInUl = document.creatElement('li');
-var newArrayDataInhours = document.createTextNode('hoursOpen');
-typeOfChicken.appendChild('newLiInUl');
-
+var ulEl = document.getElementById('create-Ul');
+var hoursCookies = [];
 var aveCustomer = function (maxCustomers,minCustomers) {
   return Math.floor(Math.random () * (maxCustomers - minCustomers + 1) + minCustomers);
 };
@@ -27,13 +12,25 @@ var pike = {
   averageCookie: 5.2,
   maxCustomers: 88,
   minCustomers: 17,
+  totalCookies: 0,
+  hoursCookies: [],
+  storeHtml: 'ctreat-ul',
   averageCookies: function () {
-    totalCookies = 0;
+    this.totalCookies = 0;
     for (var hours = 0; hours < hoursOpen.length; hours++) {
-      var aveCookies1hr = Math.round(aveCustomer(this.maxCustomers, this.minCustomers) * this.averageCookie);
-      hoursCookies.push([hoursOpen[hours],aveCookies1hr] + ' cookies ');
+      var aveCookies1hr = Math.round(Math.floor(Math.random() * (this.maxCustomers - this.minCustomers + 1) + this.minCustomers));
+      this.hoursCookies.push(aveCookies1hr);
       totalCookies += aveCookies1hr;
-      console.log(hoursCookies);
+      // console.log(this.hoursCookies);
+    }
+  },
+  renderData: function () {
+    this.averageCookies();
+    for (var i = 0; i < hoursCookies.length; i++) {
+      var newLiInUl = document.creatElement('li');
+      var ulEl = document.getElementByID (this.storeHtml);
+      newLiInUl.textContent = this.hoursCookies[i];
+      ulEl.appendChild('newLiUl');
     }
   }
 };
